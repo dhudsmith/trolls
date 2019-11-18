9a2ea0d6043bfdc2643bef3632f64a026643b33a
Hudson <dhudsmith@gmail.com>
2019-11-18 11:54:08 -0500

    completed t tests

--- analyze_watson_data.R
+++ analyze_watson_data.R
@@ -30,8 +30,43 @@ df_clean_long <-
   df_clean %>%
   melt(id.vars=c('source'))
 
+# add a logit column for emotions variables
+df_clean_long <-
+  df_clean_long %>%
+  mutate(
+    value_logit = gtools::logit(value),
+    value_logit = ifelse(value_logit < -100,-100, value_logit),
+    value_logit = ifelse(value_logit > 100,100, value_logit)
+  )
+
 ## make pictures!
+
+# emotion scores
 df_clean_long %>%
   ggplot(aes(x=value, color=source)) +
   facet_grid(rows='variable', scales='free_x') +
   geom_density()
+
+# logit transformed emotions
+df_clean_long %>%
+  ggplot(aes(x=value_logit, color=source)) +
+  facet_grid(rows='variable', scales='free_x') +
+  geom_density() +
+  scale_x_continuous(limits=c(-7,4))
+
+
+## t-tests
+
+#
+get_t_test_results <- function(df_emotion){
+  t.test(value_logit ~ source, df_emotion, na.action = na.omit)
+}
+
+df_clean_long %>% filter(variable=='anger' & !is.na(value_logit)) %>% get_t_test_results()
+df_clean_long %>% filter(variable=='disgust' & !is.na(value_logit)) %>% get_t_test_results()
+df_clean_long %>% filter(variable=='fear' & !is.na(value_logit)) %>% get_t_test_results()
+df_clean_long %>% filter(variable=='joy' & !is.na(value_logit)) %>% get_t_test_results()
+df_clean_long %>% filter(variable=='sadness' & !is.na(value_logit)) %>% get_t_test_results()
+
+
+
